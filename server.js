127c9b6fd31dc473dd9bfe8ea149a56225774298
tylergreulich <tgreul6@gmail.com>
2018-06-27 14:21:39 -0700

    mm

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+  );
   next();
 });
 
